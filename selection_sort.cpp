f2ea833f32b64bed5b1c900f2dd0bc5a446387b7
Deepak <deepak.pro931@gmail.com>
2022-11-15 22:33:13 +0530

    first commit

---
+++ selection_sort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void selectionSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+    int minIndex = i;
+
+
+
+  for(int j= i+1; j<n; j++){
+        if(arr[j] < arr[minIndex])
+        minIndex = j;
+  }
+
+       swap(arr[i], arr[minIndex]);
+    }
+
+}
+
+int main(){
+
+    int arr[5] = {3,1,9,5,3};
+
+     selectionSort(arr, 5);
+     printArray(arr, 5);
+    
+    return 0;
+}
